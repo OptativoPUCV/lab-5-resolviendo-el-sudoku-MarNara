496c99edaa2012d9c8f97550af2379da983d41b6
Your Name <you@example.com>
2025-05-14 09:05:51 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -55,7 +55,7 @@ int is_valid(Node* n){
         return 0;
       }
       ver[numero] = 1;
-      int k,p; 
+      int k = 3 * (i/3) + (j /3) ,p; 
       for(p=0;p<9;p++){
         int i=3*(k/3) + (p/3) ;
         int j=3*(k%3) + (p%3) ;
