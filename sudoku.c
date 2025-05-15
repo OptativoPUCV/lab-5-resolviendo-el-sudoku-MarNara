f84f4fe856013fe49e7585796446835c27342f27
Your Name <you@example.com>
2025-05-15 17:54:43 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -119,7 +119,7 @@ Almacene en la variable cont, la cantidad de iteraciones que realiza el algoritm
 int is_final(Node* n){
   for(int i = 0; i < 9; i++){
     for(int j = 0; j < 9; j++){
-      if(n->sudo == 0){
+      if(n->sudo[i][j] == 0){
         return 0;
       } 
     }
