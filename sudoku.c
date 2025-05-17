455d7ea14f58dc87c96afee55e8a49b60f768505
Your Name <you@example.com>
2025-05-16 21:12:18 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -237,7 +237,7 @@ Node* DFS(Node* initial, int* cont){//al parecer necesito una funcion extra que 
       current_nodo = (Node*) next(lista_adja);
     }
     free(lista_adja);
-    free(current_nodo);
+    free(new_nodo);
   }
   return NULL;
   
