7c50c2b23325c9214a81b7f65818d700a63709d7
Your Name <you@example.com>
2025-05-16 20:03:17 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -182,7 +182,7 @@ Node* DFS(Node* initial, int* cont){
   Stack* pila = createStack();//S
   push(pila, initial);
   
-  while(!is_empty(pila) != 0){
+  while(is_empty(pila) != 0){
     Node* new_nodo = (Node*)top(pila);
     pop(pila);
     
