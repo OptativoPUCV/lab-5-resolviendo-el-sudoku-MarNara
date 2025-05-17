449d7608592f60bedfcdffd661a8d0c1b1b70b88
Your Name <you@example.com>
2025-05-16 20:19:08 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -186,7 +186,7 @@ Node* DFS(Node* initial, int* cont){
     Node* new_nodo = (Node*)top(pila);
     pop(pila);
     //contar iteraciones cont
-    (*cont)++;
+    cont++;
     if(is_final(new_nodo)){
       return new_nodo;
     }
@@ -199,7 +199,7 @@ Node* DFS(Node* initial, int* cont){
       current_nodo = (Node*) next(lista_adja);
     }
     free(new_nodo);
-    free(lista_adja);
+    
   }
   return NULL;
   
